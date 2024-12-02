936278701ce389f64d49045041dffdc3b2843dee
oseji10 <57804483+oseji10@users.noreply.github.com>
2024-12-02 05:51:08 +0100

    latest updates

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,15 +7,15 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: +process.env.DB_PORT || 3306,
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '2wsxzaQ1!',
-  database: process.env.DB_DATABASE || 'rachel',
+  // host: process.env.DB_HOST || 'localhost',
+  // port: +process.env.DB_PORT || 3306,
+  // username: process.env.DB_USERNAME || 'root',
+  // password: process.env.DB_PASSWORD || '2wsxzaQ1!',
+  // database: process.env.DB_DATABASE || 'rachel',
   
-  // username: process.env.DB_USERNAME || 'dxqhghua_racheleyeemr',
-  // password: process.env.DB_PASSWORD || '2wsxzaQ1!6ytrew21!',
-  // database: process.env.DB_DATABASE || 'dxqhghua_racheleyeemr',
+  username: process.env.DB_USERNAME || 'dxqhghua_racheleyeemr',
+  password: process.env.DB_PASSWORD || '2wsxzaQ1!6ytrew21!',
+  database: process.env.DB_DATABASE || 'dxqhghua_racheleyeemr',
 
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: ['src/migrations/*.ts'],  // Use TypeScript migration files
